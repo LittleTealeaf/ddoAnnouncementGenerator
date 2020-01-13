943d690127df4a6892c43a0e3178f98f07aba55a
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2020-01-13 09:24:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ddoAnnouncementGenerator
+ 
---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+module ddoAnnouncementGenerator {
+
+	exports application;
+
+	requires javafx.base;
+	requires transitive javafx.graphics;
+	requires transitive javafx.controls;
+	requires transitive com.google.gson;
+	requires net.harawata.appdirs;
+	requires java.datatransfer;
+	requires java.desktop;
+	requires java.xml;
+}
