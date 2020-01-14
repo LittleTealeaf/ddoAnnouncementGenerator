3cecd90bddca3a8af00afb417a6accefbe13f5f9
Tea <shintaomonk@gmail.com>
2020-01-14 11:09:56 -0500

    Started edit quest

--- src/ui/EditQuest.java
+++ src/ui/EditQuest.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import classes.Quest;
 import classes.QuestInstance;
+import classes.Quests;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -16,7 +18,7 @@ public class EditQuest {
 	private static Quest q;
 	
 	public static void open(Quest quest) {
-		q = quest;
+		q = Quests.getQuest(quest);
 		if(stage != null && stage.isShowing()) stage.close();
 		stage = new Stage();
 		
@@ -30,8 +32,25 @@ public class EditQuest {
 		name.setText(q.getName());
 		name.textProperty().addListener((e,o,n) -> q.setName(n));
 		
+		Text lpack = new Text("Pack");
+		
+		TextField pack = new TextField();
+		pack.setText(q.getName());
+		pack.textProperty().addListener((e,o,n) -> q.setPack(n));
+		
 		GridPane content = new GridPane();
+		content.add(lname, 0, 0);
+		content.add(name, 1, 0);
+		content.add(lpack, 0, 1);
+		content.add(pack, 1, 1);
+		
 		
 		BorderPane page = new BorderPane();
+		page.setCenter(content);
+		
+		Scene sc = new Scene(page);
+		
+		stage.setScene(sc);
+		stage.show();
 	}
 }
