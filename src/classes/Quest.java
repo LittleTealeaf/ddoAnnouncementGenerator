532d06623fc95f8bbecfad07f3fe5d48dbe7ccce
Tea <shintaomonk@gmail.com>
2020-01-23 10:27:56 -0500

    waoeifawef

--- src/classes/Quest.java
+++ src/classes/Quest.java
@@ -71,6 +71,8 @@ public class Quest {
 	 *                 the {@link Quest quest}
 	 * @param isRaid   - Whether or not the {@link Quest} is a raid. A {@link Quest quest} is a raid if
 	 *                 it allows for 12 players to participate.
+	 *                 It is not a raid if it only allows a maximum of 6 players or hirelings to
+	 *                 enter. <i>Hirelings cannot enter raids</i>
 	 * @see QuestVersion
 	 */
 	public Quest(String name, List<QuestVersion> versions, boolean isRaid) {
@@ -84,7 +86,11 @@ public class Quest {
 	 * @param versions - List of {@link QuestVersion Quest Versions} that specify different versions of
 	 *                 the {@link Quest quest}
 	 * @param isRaid   - Whether or not the {@link Quest} is a raid. A {@link Quest quest} is a raid if
-	 *                 it allows for 12 players to participate.
+	 *                 it allows for 12 players to participate. <br>
+	 *                 It is not a raid if it only allows a maximum of 6 players or hirelings to
+	 *                 enter.<br>
+	 *                 <br>
+	 *                 <i>Hirelings cannot enter raids</i>
 	 * @param pack     - The adventure pack that the {@link Quest} is part of
 	 */
 	public Quest(String name, List<QuestVersion> versions, boolean isRaid, String pack) {
@@ -123,6 +129,7 @@ public class Quest {
 
 	/**
 	 * Creates a new Quest object with a set {@code UUID} and parameters
+	 * 
 	 * @param name     - Name to give the {@link Quest}
 	 * @param uuid     - Unique Identifier of the {@link Quest}<br>
 	 *                 Use the {@link UUID#randomUUID() UUID.randomUUID().toString()} method to generate
@@ -131,7 +138,11 @@ public class Quest {
 	 * @param versions - List of {@link QuestVersion Quest Versions} that specify different versions of
 	 *                 the {@link Quest quest}
 	 * @param isRaid   - Whether or not the {@link Quest} is a raid. A {@link Quest quest} is a raid if
-	 *                 it allows for 12 players to participate.
+	 *                 it allows for 12 players to participate. <br>
+	 *                 It is not a raid if it only allows a maximum of 6 players or hirelings to
+	 *                 enter.<br>
+	 *                 <br>
+	 *                 <i>Hirelings cannot enter raids</i>
 	 */
 	public Quest(String name, String uuid, List<QuestVersion> versions, boolean isRaid) {
 		this(name, uuid, versions, isRaid, "");
@@ -148,7 +159,11 @@ public class Quest {
 	 * @param versions - List of {@link QuestVersion Quest Versions} that specify different versions of
 	 *                 the {@link Quest quest}
 	 * @param isRaid   - Whether or not the {@link Quest} is a raid. A {@link Quest quest} is a raid if
-	 *                 it allows for 12 players to participate.
+	 *                 it allows for 12 players to participate. <br>
+	 *                 It is not a raid if it only allows a maximum of 6 players or hirelings to
+	 *                 enter.<br>
+	 *                 <br>
+	 *                 <i>Hirelings cannot enter raids</i>
 	 * @param pack     - The adventure pack that the {@link Quest} is part of
 	 */
 	public Quest(String name, String uuid, List<QuestVersion> versions, boolean isRaid, String pack) {
@@ -159,59 +174,162 @@ public class Quest {
 		this.versions = versions;
 	}
 
+	/**
+	 * Gets the name of the Quest
+	 * 
+	 * @return Name of the {@link Quest}
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name of the Quest
+	 * 
+	 * @param name - Name to give the {@link Quest}
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gets the UUID of the Quest
+	 * 
+	 * @return Unique Identifier of the {@link Quest}
+	 * @see UUID
+	 */
 	public String getUuid() {
 		return uuid;
 	}
 
+	/**
+	 * Sets the UUID of the Quest.<br>
+	 * <br>
+	 * This method will not update current references to the {@link Quest}, use with care.
+	 * 
+	 * @param uuid - Unique Identifier of the {@link Quest}<br>
+	 *             Use the {@link UUID#randomUUID() UUID.randomUUID().toString()} method to generate
+	 *             a
+	 * @see UUID
+	 */
 	public void setUuid(String uuid) {
 		this.uuid = uuid;
 	}
 
+	/**
+	 * Gets the Adventure Pack of the Quest
+	 * 
+	 * @return Adventure Pack the {@link Quest} belongs to
+	 */
 	public String getPack() {
 		return pack;
 	}
 
+	/**
+	 * Sets the Adventure Pack of the Quest
+	 * 
+	 * @param pack - The adventure pack that the {@link Quest} is part of
+	 */
 	public void setPack(String pack) {
 		this.pack = pack;
 	}
 
+	/**
+	 * Gets the Quest type
+	 * 
+	 * @return {@code true} - if the {@link Quest quest} is a raid, and allows for a total of 12 players
+	 *         to enter<br>
+	 *         {@code false} - if the {@link Quest quest} is not a raid and only allows for a maximum of
+	 *         6 players to
+	 *         join.
+	 */
 	public boolean isRaid() {
 		return isRaid;
 	}
 
+	/**
+	 * Sets whether or not the Quest is a raid
+	 * 
+	 * @param isRaid - Whether or not the {@link Quest} is a raid. A {@link Quest quest} is a raid if
+	 *               it allows for 12 players to participate. <br>
+	 *               It is not a raid if it only allows a maximum of 6 players or hirelings to
+	 *               enter.<br>
+	 *               <br>
+	 *               <i>Hirelings cannot enter raids</i>
+	 */
 	public void setRaid(boolean isRaid) {
 		this.isRaid = isRaid;
 	}
 
+	/**
+	 * Gets the complete list of all versions of the Quest
+	 * 
+	 * @return List of {@link QuestVersion Quest Versions} of the {@link Quest}
+	 * @see QuestVersion
+	 */
 	public List<QuestVersion> getVersions() {
 		return versions;
 	}
 
+	/**
+	 * Gets the Quest Version object of a particular level range
+	 * 
+	 * @param levelRange - the {@link LevelRange} of the {@link Quest}
+	 * @return the {@link QuestVersion Quest Version} of the {@link Quest} that is of that
+	 *         {@link LevelRange Level Range}
+	 * @see QuestVersion
+	 * @see LevelRange
+	 */
 	public QuestVersion getVersion(LevelRange levelRange) {
 		for(QuestVersion v : versions) if(v.getLevelRange() == levelRange) return v;
 		return null;
 	}
 
+	/**
+	 * Sets the complete list of Quest VErsions
+	 * 
+	 * @param versions - List of {@link QuestVersion Quest Versions} that specify different versions of
+	 *                 the {@link Quest quest}
+	 * @see QuestVersion
+	 */
 	public void setVersions(List<QuestVersion> versions) {
 		this.versions = versions;
 	}
 
+	/**
+	 * Adds a particular Quest Version to the list of Quest Versions
+	 * 
+	 * @param version - The specified {@link QuestVersion} of the {@link Quest} to add to the current
+	 *                list of {@link QuestVersion Quest Versions}
+	 * @see QuestVersion
+	 */
 	public void addVersion(QuestVersion version) {
 		this.versions.add(version);
 	}
 
+	/**
+	 * Removes a particular Quest Version to the list of Quest Versions
+	 * 
+	 * @param version - The specified {@link QuestVersion} of the {@link Quest} to remove from the
+	 *                current list of {@link QuestVersion Quest Versions}
+	 * @see QuestVersion
+	 */
 	public void removeVersion(QuestVersion version) {
 		this.versions.remove(version);
 	}
 
+	/**
+	 * A specific representation of the {@link Quest quest's} different difficulty levels.
+	 * <br>
+	 * <br>
+	 * <b>Variables:</b><br>
+	 * <ul>
+	 * <li>{@code levelRange} - the {@link LevelRange} of the {@code QuestVersion}</li>
+	 * </ul>
+	 * 
+	 * @author Tealeaf
+	 *
+	 */
 	public static class QuestVersion {
 
 		private LevelRange levelRange;
