34f36210055ba4054acb02ad91a2e4019f03dff5
Tealeaf <shintaomonk@gmail.com>
2020-02-11 12:35:55 -0500

    Started working on timeZone.java
    Signed-off-by: Tealeaf <ShintaoMonk@gmail.com>

--- src/uielements/ZoneSelector.java
+++ src/uielements/ZoneSelector.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.VBox;
 
 import java.time.ZoneId;
 import java.time.format.TextStyle;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -56,15 +57,53 @@ public class ZoneSelector extends VBox {
 
 	private void updateList(String filter) {
 		list.getChildren().clear();
-		for (ZoneId id : zones) { // LIST ALL ZONES IN HEREEEEE
-			//TODO if filter
-			CheckBox checkbox = new CheckBox(id.getDisplayName(TextStyle.SHORT, Locale.getDefault()));
-			checkbox.setSelected(zones.contains(id));
-			checkbox.selectedProperty().addListener((e, o, n) -> {
-				zones.remove(id);
-				if (n) zones.add(id);
-			});
-			list.getChildren().add(checkbox);
+//		for (ZoneId id : zones) { // LIST ALL ZONES IN HEREEEEE
+//			//TODO if filter
+//			CheckBox checkbox = new CheckBox(id.getDisplayName(TextStyle.SHORT, Locale.getDefault()));
+//			checkbox.setSelected(zones.contains(id));
+//			checkbox.selectedProperty().addListener((e, o, n) -> {
+//				zones.remove(id);
+//				if (n) zones.add(id);
+//			});
+//			list.getChildren().add(checkbox);
+//		}
+		for (TimeZone timeZone : TimeZone.allZones()) {
+			if (timeZone.filterText().contains(filter)) {
+				CheckBox checkbox = new CheckBox(timeZone.displayName());
+				//CUSTOM CHECK METHOD
+				//might want to save time zones by zone id and such instead of the acutal class? maybe? soemrfawef?
+			}
+		}
+	}
+
+	private static class TimeZone {
+		private ZoneId zone;
+
+		public TimeZone(ZoneId zone) {
+			this.zone = zone;
+		}
+
+		public static List<TimeZone> allZones() {
+			List<TimeZone> r = new ArrayList<TimeZone>();
+			for (String id : ZoneId.getAvailableZoneIds()) {
+				r.add(new TimeZone(ZoneId.of(id)));
+			}
+			return r;
+		}
+
+		public String displayName() {
+			return zone.getDisplayName(TextStyle.SHORT, Locale.getDefault());
+		}
+
+		public ZoneId getZone() {
+			return zone;
+		}
+
+		public String filterText() {
+			String r = " " + zone.getDisplayName(TextStyle.FULL, Locale.getDefault());
+			r += " " + zone.getDisplayName(TextStyle.FULL, Locale.getDefault());
+			r += " " + zone.getDisplayName(TextStyle.NARROW, Locale.getDefault());
+			return r;
 		}
 	}
 }
