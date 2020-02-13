32b6a522328abf4c4430894f981fa18daa34bf1b
Tealeaf <shintaomonk@gmail.com>
2020-02-13 10:24:20 -0500

    so.. gonna be like BAM there's a class with a BAMMMM zone and it's gonna be like "gimme the name" and it'll take the ID and go like "WOOT LEMME GET TEH LAST PART" and format it reall nice and it'll be like "BOOM" ID IS GENERATED!
    I need help
    
    Signed-off-by: Tealeaf <ShintaoMonk@gmail.com>

--- src/classes/Zone.java
+++ src/classes/Zone.java
@@ -1,38 +1,5 @@
 package classes;
 
-import java.time.ZoneId;
-
-public enum Zone {
-    AT(ZoneId.of("US/Alaska"), "a", "Alaska Time"),
-    ET(ZoneId.of("US/NewYork"), "e", "Eastern Time"),
-    HT(ZoneId.of("US/Hawaii"), "h", "Hawaii Time"),
-    MT(ZoneId.of("US/Mountain"), "m", "Mountain Time"),
-    PT(ZoneId.of("US/Pacific"), "p", "Pacific Time");
-
-
-    ZoneId id;
-    String shortName;
-    String longName;
-
-    Zone(ZoneId id, String shortName, String longName) {
-        this.id = id;
-        this.shortName = shortName;
-        this.longName = longName;
-    }
-
-    public ZoneId getZone() {
-        return id;
-    }
-
-    public String getNameLong() {
-        return longName;
-    }
-
-    public String getNameShort() {
-        return shortName;
-    }
-
-    public String toString() {
-        return longName;
-    }
+public class Zone {
+    //TIME TO REDO DIS
 }
